eb78acd00620defb5058e9aec53adbe9c1255cda
tangbaotrann <tangbaotrann@gmail.com>
2023-02-27 22:55:32 +0700

    updated: message socket

--- src/index.js
+++ src/index.js
@@ -28,10 +28,59 @@ const io = new Server(server, {
   },
 });
 
+/** --- HANDLE SOCKET SERVER TO CLIENT  --- */
+// arrays user
+let users = [];
+
+// add user
+const statusUser = (userId, socketId) => {
+  // check array, nếu có user đó --> true
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
 // Handle logic here
 io.on("connection", (socket) => {
-  console.log("---> A user connected... " + `${socket.id}`);
+  // console.log("---> A user connected... " + `${socket.id}`);
 
+  // user join room (room: conversation id)
+  socket.on("join_room", (room) => {
+    try {
+      console.log("[ROM] - " + `${room}`);
+      socket.join(room);
+
+      // (get users when online)
+      io.emit("get_users", users);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // status user
+  socket.on("status_user", (userId) => {
+    // console.log("---> A user connected... " + `${socket.id} -> ${userId}`);
+    try {
+      statusUser(userId, socket.id);
+
+      io.emit("get_users", users);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // send message
+  socket.on("send_message", ({ message }) => {
+    console.log("[MESSAGE] -> ", message);
+    try {
+      const { conversation } = message;
+
+      io.to(conversation).emit("receiver_message", message);
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // test socket
   io.emit("from_server", "Hello client!!!");
 });
 
