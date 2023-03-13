920f6d9ca388aa151b83ad5c86c1ed4cf93f94cb
tangbaotrann <tangbaotrann@gmail.com>
2023-03-13 21:55:45 +0700

    updated: call to user

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
 // app
@@ -14,6 +15,7 @@ dotenv.config();
 
 // middleware
 app.use(cors());
+app.use(bodyParser.json());
 
 // create server
 const server = http.createServer(app);
@@ -42,13 +44,16 @@ const statusUser = (userId, socketId) => {
 
 // Handle logic here
 io.on("connection", (socket) => {
-  // console.log("---> A user connected... " + `${socket.id}`);
+  console.log("---> A user connected... " + `${socket.id}`);
 
   // user join room (room: conversation id)
   socket.on("join_room", (room) => {
+    console.log("[ROOM]", room);
     try {
-      console.log("[ROM] - " + `${room}`);
-      socket.join(room);
+      const { _id } = room;
+
+      socket.join(_id);
+      socket.emit("joined_room", _id);
 
       // (get users when online)
       io.emit("get_users", users);
@@ -71,11 +76,93 @@ io.on("connection", (socket) => {
 
   // send message
   socket.on("send_message", ({ message }) => {
-    console.log("[MESSAGE] -> ", message);
+    // console.log("[MESSAGE] -> ", message);
     try {
       const { conversation } = message;
+      // console.log("[conversation send message]", conversation);
 
-      io.to(conversation).emit("receiver_message", message);
+      io.to(conversation).emit("receiver_message", { message });
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // rule: RULE_NOTIFICATION_REGISTER_SCHEDULE
+  socket.on("notification_confirm_register_schedule", ({ data }) => {
+    try {
+      // console.log("[NOTIFICATION REGISTER SCHEDULE] ->", data);
+      const { notification } = data;
+
+      io.to(notification.to).emit(
+        "notification_confirm_register_schedule_success",
+        notification
+      );
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // rule: RULE_NOTIFICATION_CANCEL_SCHEDULE
+  socket.on("notification_confirm_cancel_schedule", ({ data }) => {
+    try {
+      // console.log("[NOTIFICATION CANCEL SCHEDULE] ->", data);
+      const { notification } = data;
+
+      io.to(notification.to).emit(
+        "notification_confirm_cancel_schedule_success",
+        notification
+      );
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // rule: RULE_DOCTOR_REMIND
+  socket.on("notification_doctor_remind", ({ data }) => {
+    // console.log("[NOTIFICATION DOCTOR REMIND] ->", data);
+
+    try {
+      const { notification } = data;
+
+      io.to(notification.to).emit(
+        "notification_doctor_remind_success",
+        notification
+      );
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // register schedule from patient
+  socket.on("notification_register_schedule_from_patient", ({ data }) => {
+    // console.log("notification ->", data);
+    try {
+      // console.log("[NOTIFICATION- REGISTER SCHEDULE FROM PATIENT] -> ", data);
+      // console.log(
+      //   "[CONVERSATION- REGISTER SCHEDULE FROM PATIENT] -> ",
+      //   conversation
+      // );
+
+      // emit
+      io.to(data.to).emit(
+        "notification_register_schedule_from_patient_success",
+        data
+      );
+    } catch (err) {
+      console.log({ err });
+    }
+  });
+
+  // call id room to user
+  socket.on("call_id_room_to_user", ({ conversation, infoDoctor }) => {
+    console.log("[conversation ID]", conversation);
+    console.log("[infoDoctor]", infoDoctor);
+
+    try {
+      io.to(conversation.member._id).emit("call_id_room_to_user_success", {
+        room_id: conversation._id,
+        doctor_username: infoDoctor.person.username,
+      });
     } catch (err) {
       console.log({ err });
     }
